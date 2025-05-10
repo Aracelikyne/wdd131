0ee00cb26d7a2aea9e1ea2a5ca92e65c6066ccd2
Monica "Araceli" Kyne <aracelikyne@gmail.com>
2025-05-10 16:21:25 -0500

    test push

---
+++ scripts/getdates.js
@@ -0,0 +1,19 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+
+  
+    const currentYear = new Date().getFullYear();
+    const yearSpan = document.getElementById('currentyear');
+    if (yearSpan) {
+        yearSpan.textContent = currentYear;
+    }
+
+
+    const lastModifiedDate = document.lastModified;
+    const lastModifiedParagraph = document.getElementById('lastModified');
+    if (lastModifiedParagraph) {
+
+        lastModifiedParagraph.textContent = "Last Modified: " + lastModifiedDate;
+    }
+
+});
